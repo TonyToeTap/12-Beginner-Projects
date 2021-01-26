45a8a1a257a33cf33778c0824d3719023c597e45
TonyToeTap <lukasjunger@MacBook-Pro-143.fritz.box>
2021-01-26 18:10:20 +0100

    Hangman Projekt abgeschlossen

--- hangman.py
+++ hangman.py
@@ -1,5 +1,52 @@
 import random as rd
 from words import words
+import string
 
-word = rd.choice(words)
-print(word)
+
+def get_valid_word(words):
+    word = rd.choice(words)
+    while '-' in word or ' ' in word:
+        word = rd.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what the current word is
+        word_list = [letter if letter in used_letters else '_' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives -1
+                print('Letter is not in word.')
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    print('You guessed the word', word, '!!')
+
+hangman()
+
+
+
